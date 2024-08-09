94aea8b05a6c833380c7490ec16f4031568a34e8
Pixel Pirates <tobauntat@gmail.com>
2024-08-09 09:55:08 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,8 +21,8 @@ export default function Hero({ english }) {
         </Typography>
         <Typography variant="p" component="p" sx={{ margin: 5 }}>
           {english
-            ? "With over a decade of experience in software development, I have combined independent projects with formal training at Techover. During my time there, I refined my skills by developing a range of complex applications. Examples of projects include a password generator, an integration with Klarna Checkout and a comprehensive flag application."
-            : "Med över ett decennium av erfarenhet inom mjukvaruutveckling, har jag kombinerat självständiga projekt med en formell utbildning hos Techover. Under min tid där har jag förfinat mina kunskaper genom att utveckla en rad komplexa applikationer. Exempel på projekt inkluderar en lösenordsgenerator, en integration med Klarna Checkout och en omfattande flagg-applikation."}
+            ? "After over a decade of passionate hobby coding, I decided to take my software development to the next level. Through intensive training at Techover, I was able to deepen my knowledge and develop a range of complex applications. Among other things, I created an extensive Spotify clone using the Spotify API, which required in-depth knowledge of API integrations, database management and user interfaces. In addition to this, I developed a password generator, an integration with Klarna Checkout and a comprehensive flag application."
+            : "Efter över ett decennium av passionerat hobby-kodande, bestämde jag mig för att ta min mjukvaruutveckling till nästa nivå. Genom en intensiv utbildning hos Techover kunde jag fördjupa mina kunskaper och utveckla en rad komplexa applikationer. Bland annat skapade jag en omfattande Spotify-klon med hjälp av Spotify API, vilket krävde djupgående kunskaper inom API-integrationer, databashantering och användargränssnitt. Utöver detta utvecklade jag en lösenordsgenerator, en integration med Klarna Checkout och en omfattande flagg-applikation."}
         </Typography>
       </Box>
     </Box>
